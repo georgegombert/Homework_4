ca98ea43bf7a8764c17227d382c199495e74eb75
george-g <georgegombert@gmail.com>
2020-03-23 21:35:43 -0600

    Initail upload including framework HTML and js variables

---
+++ assets/main.js
@@ -0,0 +1,23 @@
+var questions = {
+    question1: "Commonly used data types DO NOT include:",
+    question2: "The condition in an if/else statementis enclosed within _____.",
+    question3: "Arrays in JavaScript can be used to store _____.",
+    question4: "String values must be enclosed within ___ when being assigned to variables",
+    question5: "A useful tool durring development and debugging for printing content to the debugger is:"
+};
+
+var answers = {
+    answer1: ["1. strings", "2. booleans","3. alerts","4. numbers"],
+    answer2: ["1. quotes", "2. curley brackets","3. parenthesis","4. square brackets"],
+    answer3: ["1. numbers and strings", "2. other arrays","3. booleans","4. all of the above"],
+    answer4: ["1. commas", "2. curley brackets","3. quotes","4. parentheses"],
+    answer5: ["1. JavaScript", "2. terminal/bash","3. for loops","4. consol.log"]
+};
+
+var rightAnswer = {
+    rightAnswer1: "3. alerts",
+    rightAnswer2: "3. parenthesis",
+    rightAnswer3: "4. all of the above",
+    rightAnswer4: "3. quotes",
+    rightAnswer5: "4. consol.log"
+};
